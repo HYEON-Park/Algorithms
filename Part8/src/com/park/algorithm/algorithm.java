4b7c396ce89353665319dbb4bd481985eb5f8f62
Hyeon Park <hyeonpaaark@gmail.com>
2021-05-16 20:45:12 +0900

    Part10_algorithm
    거듭제곱구하기  / for

---
+++ Part8/src/com/park/algorithm/algorithm.java
@@ -0,0 +1,21 @@
+package com.park.algorithm;
+
+public class algorithm {
+	
+	public static void main(String[] args) {
+		
+		int n = 5;
+		int m = 3;
+		int a = 1;
+		//n의 m승을 구하시오
+		
+		for (int i = 0; i < m; i++) {	//012
+//			System.out.println(m);
+			a *= n;
+		}
+		System.out.println(a);
+	}
+
+	
+	
+ }
