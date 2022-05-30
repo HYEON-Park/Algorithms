c7b18069cb7732e6f9827439d956c213bc6e0d65
HYEONP--Park <hyeonpaaark@gmail.com>
2022-05-30 21:45:14 +0900

    두잇잡바알고리즘 1

---
+++ Algorithm_2205/src/Test1_1-2.java
@@ -0,0 +1,13 @@
+class test {
+    static int max3(int a, int b, int c) {
+
+        int max = a;
+
+        if (b > max)
+            max = b;
+        if (c > max)
+            max = c;
+
+        return max;
+    }
+}
