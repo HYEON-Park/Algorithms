d9637b6b6baa6c7855d987255790fd42547ebd11
HYEONP--Park <hyeonpaaark@gmail.com>
2022-06-09 21:50:22 +0900

    알고리즘 입문 실습문제 3-1

---
+++ Algorithm_2205/src/SeqSearch.java
@@ -0,0 +1,39 @@
+package Algorithm_2205.src;
+
+import java.util.Scanner;
+
+class SeqSearch {
+    // 요솟수가 n인 배열 a에서 key와 같은 요소를 선형 검색합니다.
+
+    static int SeqSearch(int[] a, int n, int key) {
+        int i = 0;
+
+        while (true) {
+            if (i == n) {
+                if (i == n) {
+                    return -1;
+                }
+                if (a[i] == key) {
+                    return i;
+                }
+                i++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("요솟수 : ");
+
+        int num = sc.nextInt();
+        int[] x = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            System.out.println("x[" + i + "]:");
+            x[i] = sc.nextInt();
+        }
+        System.out.println("검색할 값 : ");
+        int ky = sc.nextInt();
+    }
+}
